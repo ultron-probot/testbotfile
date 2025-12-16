edf082b6d6735adc5d977f1b1a2cb247c3aee9b2
ultron-probot <smitjhn392@gmail.com>
2025-12-16 13:03:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ API_ID = int(os.environ.get("API_ID", 123456))
 API_HASH = os.environ.get("API_HASH", "API_HASH_HERE")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "BOT_TOKEN_HERE")
 
-OWNER_ID = int(os.environ.get("OWNER_ID", 123456789))
-LOG_GROUP = int(os.environ.get("LOG_GROUP", -100123456789))
+OWNER_ID = int(os.environ.get("OWNER_ID", 8404535682)
+LOG_GROUP = int(os.environ.get("LOG_GROUP", -1003415074133))
 
 START_IMAGE = os.environ.get(
     "START_IMAGE",
-    "https://telegra.ph/file/example.jpg"
+    "https://files.catbox.moe/8a3dum.jpg"
 )
 
-JOIN1 = os.environ.get("JOIN1", "")
-JOIN2 = os.environ.get("JOIN2", "")
-JOIN3 = os.environ.get("JOIN3", "")
+JOIN1 = os.environ.get("JOIN1", "https://t.me/A2globalupdate")
+JOIN2 = os.environ.get("JOIN2", "https://t.me/A2globalupdate")
+JOIN3 = os.environ.get("JOIN3", "https://t.me/A2globalupdate")
